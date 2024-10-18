e6a6ae5f1456bfcc9dca7e0bb6b82cc424174c10
Vkorchagin82 <victoria.korchagin@gmail.com>
2024-10-18 13:10:25 -0400

    updated scrape

--- HistoricalCandleDataTimelySplit.py
+++ HistoricalCandleDataTimelySplit.py
@@ -5,19 +5,36 @@ import pandas as pd
 from datetime import datetime, timedelta
 import pytz
 from pandas import json_normalize
+import argparse
 
 base_url = 'api.coinbase.com'
 scrape_path = "/api/v3/brokerage/market/products/BTC-USD/candles?start={0}&end={1}&granularity={2}&limit={3}"
 
 
-def convert_time_est_to_unix(est_time):
+def parse_args():
+    parser = argparse.ArgumentParser(description="Greet the user with their name and age.")
+
+    # Add arguments
+    parser.add_argument("start_date", type=str, help="start date in format 'MM/DD/YYYY")
+    parser.add_argument("end_date", type=str, help="end_date in format 'MM/DD/YYYY")
+    parser.add_argument("start_time", type=str, help="start time in format 'HH:mm:ss'")
+    parser.add_argument("end_date", type=str, help="end time in format 'HH:mm:ss'")
+    parser.add_argument("time_interval", type=str, choices=['ONE_HOUR', 'ONE_DAY'], help="time interval")
+    parser.add_argument("no_of_items", type=int, help="number of items")
+
+    # Parse the arguments
+    return parser.parse_args()
+
+
+def convert_time_est_to_unix(est_time: datetime):
     est = pytz.timezone('US/Eastern')
     localized_time = est.localize(est_time)
     utc_time = localized_time.astimezone(pytz.utc)
     return int(utc_time.timestamp())
 
 
-def get_bitcoin_hourly_prices(est_start_date, est_end_date, start_time, end_time, granularity='ONE_HOUR', limit=12):
+def get_bitcoin_hourly_prices(est_start_date: str, est_end_date: str, start_time: str, end_time: str,
+                              granularity='ONE_HOUR', limit=12):
     data_list = pd.DataFrame(columns=["start", "low", "high", "open", "close", "volume"])
     start_date = datetime.strptime(est_start_date, '%m/%d/%Y')
     end_date = datetime.strptime(est_end_date, '%m/%d/%Y')
@@ -65,4 +82,6 @@ def save_data(data_list):
 
 
 if __name__ == '__main__':
-    get_bitcoin_hourly_prices('09/26/2024', '09/01/2023', '00:00:0', '23:59:59', 'ONE_HOUR', 25)
+    args  = parse_args()
+    get_bitcoin_hourly_prices(args.start_date, args.end_date,
+                              args.start_time, args.end_time, args.time_interval, args.no_of_items)
